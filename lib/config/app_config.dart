b76efdef0eb55dcc70d77ea9a4be1663229712c0
abhi <anshabhi7926@gmail.com>
2026-06-07 12:19:11 +0530

    updated render path

--- lib/config/app_config.dart
+++ lib/config/app_config.dart
@@ -1,7 +1,7 @@
 class AppConfig {
   static const String apiBaseUrl = String.fromEnvironment(
     'PANCHANG_API_BASE_URL',
-    defaultValue: 'http://localhost:8080',
+    defaultValue: 'https://panchang-mo63.onrender.com',
   );
 
 }
